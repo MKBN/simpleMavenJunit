ac8d6458a8417be29e8c7c8d657f7d1e28786cc1
MKBN <40356725+MKBN@users.noreply.github.com>
2021-11-14 10:41:17 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Mohan Kumar BN !! Hi or Bye hello is the string needed here.";
+    return "Hello world- This is Mohan Kumar BN !! Hi or ye hello is the string needed here.";
   }
 }
